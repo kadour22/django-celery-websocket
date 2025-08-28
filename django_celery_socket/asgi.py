8d98561c6fc333295ee73e5a7d4837af19ea1e94
kadour <kadourzaroui399@gmail.com>
2025-08-28 11:49:20 -0400

    install & configure websocket

--- django_celery_socket/asgi.py
+++ django_celery_socket/asgi.py
@@ -1,16 +1,15 @@
-"""
-ASGI config for django_celery_socket project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
-"""
-
 import os
 
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_celery_socket.settings')
+django_application = get_asgi_application()
 
-application = get_asgi_application()
+import core.routing 
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": URLRouter(core.routing.websocket_urlpatterns)
+})
--- django_celery_socket/settings.py
+++ django_celery_socket/settings.py
@@ -122,3 +122,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CHANNEL_LAYERS = {
+    "default" : {"BACKEND": "channels.layers.InMemoryChannelLayer"},
+}
